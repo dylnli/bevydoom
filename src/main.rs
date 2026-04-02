e1d181b1a3d3589d8c16857b9f9c5a8a15969aff
squidgyberries <squidgyberries@outlook.com>
2026-04-02 10:46:51 +0800

    testing color palette and floor texture

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ mod wad;
 
 use anyhow::anyhow;
 use bevy::{
+    asset::RenderAssetUsages,
     camera_controller::free_camera::{FreeCamera, FreeCameraPlugin},
     prelude::*,
+    render::render_resource::{Extent3d, TextureDimension},
     window::WindowResolution,
     winit::WinitSettings,
 };
@@ -37,10 +39,37 @@ fn main() {
         .run();
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct DoomColor {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl DoomColor {
+    pub const BLACK: Self = Self::new(0, 0, 0);
+    pub const WHITE: Self = Self::new(1, 1, 1);
+
+    pub const fn new(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
+    }
+}
+
+struct DoomPalette {
+    array: [DoomColor; 256],
+}
+
+impl DoomPalette {
+    pub fn new(array: [DoomColor; 256]) -> Self {
+        Self { array }
+    }
+}
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut images: ResMut<Assets<Image>>,
     args: Res<CommandLineArgs>,
 ) -> Result {
     if args.0.len() < 2 {
@@ -49,13 +78,83 @@ fn setup(
 
     let wad = WadFile::load(&args.0[1])?;
 
+    let playpal = wad.load_lump(wad.find_lump(WadName::from("PLAYPAL")).unwrap());
+    let first_palette = &playpal[0..768];
+    let mut palette_array = [DoomColor::BLACK; 256];
+    let mut palette_formatted = Vec::with_capacity(1024);
+    for i in 0..first_palette.len() / 3 {
+        let r = first_palette[i * 3];
+        let g = first_palette[i * 3 + 1];
+        let b = first_palette[i * 3 + 2];
+        palette_formatted.push(r);
+        palette_formatted.push(g);
+        palette_formatted.push(b);
+        palette_formatted.push(255);
+        palette_array[i] = DoomColor::new(r, g, b);
+    }
+    let doom_palette = DoomPalette::new(palette_array);
+    let palette_image = Image::new(
+        Extent3d {
+            width: 16,
+            height: 16,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        palette_formatted,
+        bevy::render::render_resource::TextureFormat::Rgba8UnormSrgb,
+        RenderAssetUsages::RENDER_WORLD,
+    );
+    let palette_handle = images.add(palette_image);
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(64.0, 64.0, 64.0))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::WHITE,
+            unlit: true,
+            base_color_texture: Some(palette_handle),
+            ..default()
+        })),
+    ));
+
     let map = Map::load(&wad, WadName::from("E1M2")).unwrap();
+
+    let sector = map.sectors[0];
+    let floor_flat = wad.load_lump(wad.find_lump(sector.floor_texture).unwrap());
+    let mut image_data = Vec::with_capacity(64 * 64 * 4);
+    for pixel in floor_flat {
+        let color = doom_palette.array[*pixel as usize];
+        image_data.push(color.r);
+        image_data.push(color.g);
+        image_data.push(color.b);
+        image_data.push(255);
+    }
+    let flat_image = Image::new(
+        Extent3d {
+            width: 64,
+            height: 64,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        image_data,
+        bevy::render::render_resource::TextureFormat::Rgba8Unorm,
+        RenderAssetUsages::RENDER_WORLD,
+    );
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(64.0, 64.0))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::WHITE,
+            base_color_texture: Some(images.add(flat_image)),
+            unlit: true,
+            ..default()
+        })),
+        Transform::from_xyz(0.0, 100.0, 0.0),
+    ));
 
     commands.insert_resource(wad);
 
     let unlit_material = materials.add(StandardMaterial {
         base_color: Color::WHITE,
         unlit: true,
+        // base_color_texture: Some(palette_handle),
         // cull_mode: None,
         ..default()
     });
